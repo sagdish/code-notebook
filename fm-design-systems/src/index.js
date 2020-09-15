e5de1e4d80b5faca550ae0eb52f798d4bbbb12d0
sagdish <forsi1@yahoo.com>
2020-09-14 20:09:22 -0700

    FM ds themes

--- fm-design-systems/src/index.js
+++ fm-design-systems/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <h1>Hello Boo Boo Baa Baa</h1>;
+import PrimaryButton, {
+  SecondaryButton,
+  TertiaryButton,
+} from "./components/Buttons";
 
+const App = () => (
+  <>
+    <PrimaryButton>Hello Boo Boo Baa Baa</PrimaryButton>
+    <SecondaryButton>Hello Boo Boo Baa Baa</SecondaryButton>
+    <TertiaryButton>Hello Boo Boo Baa Baa</TertiaryButton>
+  </>
+);
 ReactDOM.render(<App />, document.getElementById("root"));
