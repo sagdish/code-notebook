e282182c82a5cee76d4e1e4329a917f84cf99170
sagdish <forsi1@yahoo.com>
2020-09-15 17:24:50 -0700

    buttons state design

--- fm-design-systems/src/index.js
+++ fm-design-systems/src/index.js
@@ -5,12 +5,18 @@ import PrimaryButton, {
   SecondaryButton,
   TertiaryButton,
 } from "./components/Buttons";
+import { GlobalStyle } from "./components/utils";
 
 const App = () => (
-  <div>
+  <>
     <PrimaryButton>Hello Boo Boo Baa Baa</PrimaryButton>
     <SecondaryButton>Hello Boo Boo Baa Baa</SecondaryButton>
     <TertiaryButton>Hello Boo Boo Baa Baa</TertiaryButton>
-  </div>
+
+    <PrimaryButton disabled>disabled</PrimaryButton>
+    <SecondaryButton disabled>disabled</SecondaryButton>
+    <TertiaryButton disabled>disabled</TertiaryButton>
+    <GlobalStyle />
+  </>
 );
 ReactDOM.render(<App />, document.getElementById("root"));
