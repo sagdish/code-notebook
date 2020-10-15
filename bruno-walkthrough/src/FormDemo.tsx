d20b9acb575af90612ccc59c8146bec106da5f6d
sagdish <forsi1@yahoo.com>
2020-10-14 20:39:52 -0700

    beautify with Formgroup and Box from material ui

--- bruno-walkthrough/src/FormDemo.tsx
+++ bruno-walkthrough/src/FormDemo.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Checkbox, CheckboxProps, FormControlLabel, MenuItem, TextField, Typography} from '@material-ui/core';
+import { Box, Card, CardContent, Checkbox, CheckboxProps, FormControlLabel, FormGroup, MenuItem, TextField, Typography} from '@material-ui/core';
 import React from 'react';
 import { Form, Formik, Field, useField } from 'formik';
 import { InvestmentDetails } from './InvestmentDetails';
@@ -22,25 +22,42 @@ export function FormDemo() {
         <Formik initialValues={initialValues} onSubmit={() => {}}>
           {({ values }) => (
             <Form>
-              <Field name="fullName" as={TextField} label="Full Name" />
-              <Field name="initialInvestment" type="number" as={TextField} label="Initial Investment"/>
+              <Box marginBottom={2}>
+                <FormGroup>
+                  <Field name="fullName" as={TextField} label="Full Name" />
+                </FormGroup>
+              </Box>
+
+              <Box marginBottom={2}>
+                <FormGroup>
+                  <Field name="initialInvestment" type="number" as={TextField} label="Initial Investment"/>
+                </FormGroup>
+              </Box>
 
               <CustomCheckbox name="investmentRisk" value="High" label="High" />
               <CustomCheckbox name="investmentRisk" value="Medium" label="Medium" />
               <CustomCheckbox name="investmentRisk" value="Low" label="Low" />
               
-              <Field name="commentAboutInvestmentRisk" as={TextField} multiline rows={3} rowsMax={5} />
+              <FormGroup>
+                <Field name="commentAboutInvestmentRisk" as={TextField} multiline rows={3} rowsMax={5} />
+              </FormGroup>
               
-              <Field name="dependents" as={TextField} select>
-                <MenuItem value={0}>0</MenuItem>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-              </Field>
+              <Box marginBottom={2}>
+                <FormGroup>
+                  <Field name="dependents" as={TextField} select>
+                    <MenuItem value={0}>0</MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Field>
+                </FormGroup>
+              </Box>
 
-              <CustomCheckbox name="acceptedTermsAndConditions" label="Accept Terms & Conditions" />
+              <Box marginBottom={2}>
+                <CustomCheckbox name="acceptedTermsAndConditions" label="Accept Terms & Conditions" />
+              </Box>
 
               <pre>{JSON.stringify(values, null, 4)}</pre>
             </Form>
