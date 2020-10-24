90876c8f2a4baed4c0f8a9118c6b2ee72ab60813
sagdish <forsi1@yahoo.com>
2020-10-23 18:39:14 -0700

    dom selectors and apply/change wasy

---
+++ myNotebook/DOM.js
@@ -0,0 +1,41 @@
+/* 
+                    Head ... 
+Document -> Root -> 
+                    Body ...
+
+*/
+
+
+
+// commont used ones: 
+/*
+
+document.all -> returns array like elements (html collections) of all elements
+document.title - title
+
+selectors: 
+let id = document.getElementById("id-here")
+let elements = document.getElementsByClassName('class name here') -> returns html collection
+let tags = document.getElementsByTagName('tags names')
+let qSelector = document.querySelector(".class #id") -> return one element
+let qSelectorAll = document.querySelectorAll(".class #id") -> return html collection
+
+
+apply methods:
+id.textContent = 'changed text goes here' // respects styles such as display: none
+id.innerText = "text" 
+id.innerHTML = <h2>Hello</h2> // puts html inside
+id.style.-css properties here-  = 
+id.style.borderBottom = "solid 1px red"
+
+// loop throug collection: 
+for (let i = 0; i < elements.length; i++) {
+  elements[i].style.backgroundColor = '#323424';
+}
+
+
+
+// traverse DOM:
+
+
+*/
