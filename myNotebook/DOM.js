c99aa1e76b0f7fc94d4206efb6a42bdcb1453404
sagdish <forsi1@yahoo.com>
2020-10-23 19:05:43 -0700

    create elemets, change content

--- myNotebook/DOM.js
+++ myNotebook/DOM.js
@@ -13,7 +13,7 @@ Document -> Root ->
 document.all -> returns array like elements (html collections) of all elements
 document.title - title
 
-selectors: 
+** selectors: 
 let id = document.getElementById("id-here")
 let elements = document.getElementsByClassName('class name here') -> returns html collection
 let tags = document.getElementsByTagName('tags names')
@@ -21,7 +21,7 @@ let qSelector = document.querySelector(".class #id") -> return one element
 let qSelectorAll = document.querySelectorAll(".class #id") -> return html collection
 
 
-apply methods:
+** apply methods:
 id.textContent = 'changed text goes here' // respects styles such as display: none
 id.innerText = "text" 
 id.innerHTML = <h2>Hello</h2> // puts html inside
@@ -35,7 +35,46 @@ for (let i = 0; i < elements.length; i++) {
 
 
 
-// traverse DOM:
+** changing, selecting DOM:
+var id = document.querySelector("#id");
+
+//parent node property
+id.parentNode > returns parent element
+id.parentNode.style.backgroundColor = 'fdasfd'
+id.parentNode.parentNode -> parent of parent
+
+// parent elements:
+id.parentElement -> for the most part can be interchangeble with parentNode
+
+// childs:
+bad: id.childNotes -> node list ! not used commonly
+
+id.children -> collection of element ! better method
+id.firstElementChild -> element
+id.lastElementChild -> element
+
+// siblings:
+id.nextElementSibling -> element
+id.previousElementSibling -> element
+
+
+** create elements:
+
+const newElem = document.createElement("div"); -> creates new div
+
+newElem.className = "class-name"
+newElem.id = "id-name"
+
+// add atribute
+newElem.setAttribute('title', 'hello div');
+
+// create text node:
+cosnt newDivText = document.createTextNode("Hello World");
+newElem.appendChild(newDivText);
+
+// insert element:
+someElem.insertBefore('what we insert ie div', 'before what ie h1');
+
 
 
 */
