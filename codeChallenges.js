875b038bf81eb9b6ffbb03c8715b2e2daaf051e9
sagdish <forsi1@yahoo.com>
2020-11-10 22:01:26 -0800

    comments for new code challenge

--- codeChallenges.js
+++ codeChallenges.js
@@ -49,3 +49,9 @@ function allLongestStrings(inputArray) {
 }
 
 // solve this: A Sawtooth Sequence 
+
+
+// solve this: 
+// Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+// Input strings will always be well-formed with matching ()s.
