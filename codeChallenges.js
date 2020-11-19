8e49b366e94b9d62dd5c432dc8ff2ece0cd736a6
sagdish <forsi1@yahoo.com>
2020-11-18 19:48:08 -0800

    code challenge description

--- codeChallenges.js
+++ codeChallenges.js
@@ -64,6 +64,10 @@ function alternatingSums(a) {
 
 // Input strings will always be well-formed with matching ()s.
 
+
+// Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
+// Given two arrays a and b, check whether they are similar.
+
 // Given a rectangular matrix of characters, add a border of asterisks(*) to it:
 function addBorder(picture) {
   const res = [];
