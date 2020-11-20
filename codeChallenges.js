4e162070ea63a56ed2f7c82608251f92fbc5600f
sagdish <forsi1@yahoo.com>
2020-11-19 23:56:34 -0800

    code challenge description

--- codeChallenges.js
+++ codeChallenges.js
@@ -68,6 +68,12 @@ function alternatingSums(a) {
 // Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays.
 // Given two arrays a and b, check whether they are similar.
 
+
+// You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
+// Example
+// For inputArray = [1, 1, 1], the output should be
+// arrayChange(inputArray) = 3.
+
 // Given a rectangular matrix of characters, add a border of asterisks(*) to it:
 function addBorder(picture) {
   const res = [];
