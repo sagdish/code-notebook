02799d18ffe1460c97cd6493624ff3701ce4e14a
sagdish <forsi1@yahoo.com>
2020-11-25 18:47:40 -0800

    arrayChange CC solution

--- codeChallenges.js
+++ codeChallenges.js
@@ -78,7 +78,22 @@ function areSimilar(a, b) {
 // You are given an array of integers. On each move you are allowed to increase exactly one of its element by one. Find the minimal number of moves required to obtain a strictly increasing sequence from the input.
 // Example
 // For inputArray = [1, 1, 1], the output should be
-// arrayChange(inputArray) = 3.
+// arrayChange(inputArray) = 3
+function arrayChange(inputArray) {
+  let moves = 0
+  
+  for (let i = 1; i < inputArray.length; i++) {
+      
+      if (inputArray[i] <= inputArray[i - 1]) {
+          const diff = (inputArray[i - 1] - inputArray[i]) + 1
+          moves += diff;
+          inputArray[i] += diff;
+          loop = true; 
+      }
+  }
+  
+  return moves
+}
 
 
 
