e4b724239ddd3d84596102b88ca6228f79ea6796
Sagdi Formanov <forsi1@yahoo.com>
2021-04-10 07:11:46 +0500

    metadata, title inside head and Image components

--- next/resetlist/components/Navbar.js
+++ next/resetlist/components/Navbar.js
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/shelter-logo.png" 
+          width={98}
+          height={98}
+        />
       </div>
       <Link href="/" ><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
--- next/resetlist/pages/index.js
+++ next/resetlist/pages/index.js
@@ -1,15 +1,22 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Index page</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Link href="/people">
-        <a className={styles.btn}>See ppl's listings</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>People's list | Home</title>
+        <meta name="keywords" content="people" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Index page</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Link href="/people">
+          <a className={styles.btn}>See ppl's listings</a>
+        </Link>
+      </div>
+    </>
   )
 }
