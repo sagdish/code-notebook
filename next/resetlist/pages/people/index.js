af901029dca729f6b041f65614e1a8f5661e404d
Sagdi Formanov <forsi1@yahoo.com>
2021-04-10 07:43:42 +0500

    fetching static data

--- next/resetlist/pages/people/index.js
+++ next/resetlist/pages/people/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import styles from '../../styles/People.module.css'
 
-export default function People() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: {
+      people: data
+    }
+  }
+}
+
+export default function People({ people }) {
   return (
     <div>
       <h1>All people space</h1>
+      {people.map(person => (
+        <div key={person.id}>
+          <a className={styles.single}>
+            <h3>
+              {person.name}
+            </h3>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
