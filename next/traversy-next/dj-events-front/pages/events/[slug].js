a552596819366c09028153ddf579e8b1076c64c4
Sagdi Formanov <forsi1@yahoo.com>
2021-04-27 00:23:14 +0500

    router testing and Link modules

--- next/traversy-next/dj-events-front/pages/events/[slug].js
+++ next/traversy-next/dj-events-front/pages/events/[slug].js
@@ -1,7 +1,17 @@
+import {useRouter} from 'next/router'
+
 export default function EventPage() {
+  const router = useRouter();
+
+  console.log(router)
+
   return (
     <div>
       <h1>My Event</h1>
+      <h3>you typed {router.query.slug} query</h3>
+      <button onClick={() => {
+        router.push('/')
+      }}>Go Home</button>
     </div>
   )
 }
