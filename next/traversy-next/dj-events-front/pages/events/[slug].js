bc696e26af7c0720c0a43b83bc82f28ba01dc2bf
Sagdi Formanov <forsi1@yahoo.com>
2021-04-28 11:49:49 +0500

    Layout component

--- next/traversy-next/dj-events-front/pages/events/[slug].js
+++ next/traversy-next/dj-events-front/pages/events/[slug].js
@@ -1,14 +1,14 @@
-import {useRouter} from 'next/router'
+// import {useRouter} from 'next/router'
 
 export default function EventPage() {
   const router = useRouter();
   return (
     <div>
       <h1>My Event</h1>
-      <h3>you typed {router.query.slug} query</h3>
+      {/* <h3>you typed {router.query.slug} query</h3>
       <button onClick={() => {
         router.push('/')
-      }}>Go Home</button>
+      }}>Go Home</button> */}
     </div>
   )
 }
