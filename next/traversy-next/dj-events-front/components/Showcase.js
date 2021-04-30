cb81bf6ec9fa5780e4f4ea628067e31f6dfa492b
Sagdi Formanov <forsi1@yahoo.com>
2021-04-30 16:28:16 +0500

    add events

---
+++ next/traversy-next/dj-events-front/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party</h1>
+      <h1>Find the Hottest DJ Events</h1>
+    </div>
+  )
+}
