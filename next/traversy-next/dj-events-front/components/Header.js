dd06c2607e7afce3e59121a1916e007bb2ad30ef
Sagdi Formanov <forsi1@yahoo.com>
2021-05-21 22:48:36 +0500

    register and login page

--- next/traversy-next/dj-events-front/components/Header.js
+++ next/traversy-next/dj-events-front/components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import {FaSignInAlt, FaSignOutAlt} from 'react-icons/fa'
 import Search from './Search'
 import styles from '@/styles/Header.module.css'
 
@@ -24,6 +25,11 @@ export default function Header() {
           <li>
             <Link href='/events/add'>
               <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/account/login'>
+              <a className='btn-secondary btn-icon'><FaSignInAlt /> Login</a>
             </Link>
           </li>
         </ul>
