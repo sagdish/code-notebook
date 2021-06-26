50f1fb3d494cd3e43402a4fd9a9508a4b77c0a50
Sagdi Formanov <forsi1@yahoo.com>
2021-06-26 17:21:14 +0500

    catch up

---
+++ myNotebook/arrayMethods.js
@@ -0,0 +1,18 @@
+/*
+ 
+Array.prototype.splice()
+
+Removes or replace element in place
+
+ex:
+arr.splice(position, delete count, element)
+
+
+Array.prototype.slice()
+
+returns shallow copy of an array without modifying original one
+
+slice(start: inclusive, finish: noninclusive)
+
+*/
+
