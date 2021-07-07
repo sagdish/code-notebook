b6928dd94dbf671968b16e1eb1527892ddff9b55
Sagdi Formanov <forsi1@yahoo.com>
2021-07-07 22:15:17 +0500

    css transform translate functions

--- CSS/css-ui-page/pages/index.js
+++ CSS/css-ui-page/pages/index.js
@@ -29,6 +29,30 @@ export default function Home() {
         <div id="ease-in-out"className={styles.move}>ease in out</div>
         <div id="ease-out" className={styles.move}>ease out</div>
       </div>
+
+      <div>
+        <h2>Transform</h2>
+        <p>Transform has Four functions:
+          <br/>
+          scale(number) resize elements
+          <br/>
+          translate(horizontal num, vertical num) move element, change position
+          <br/>
+          rotate(degree num) rotate element 
+          <br/>
+          skew(x axis, y axis) skew element
+          <br/>
+          usage: 
+          <br/>
+          transform: translate(value)
+
+          <div className={`${styles.box} ${styles.translate}`}>Transform</div>
+          <div className={`${styles.box} ${styles.scale}`}>Scale</div>
+          <div className={`${styles.box} ${styles.rotate}`}>Rotate</div>
+          <div className={`${styles.box} ${styles.skew}`}>Skew</div>
+
+        </p>
+      </div>
     </div>
   )
 }
