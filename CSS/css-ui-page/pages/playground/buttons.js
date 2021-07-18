eb70fb7df1b14691af768afdb371f6532beb1789
Sagdi Formanov <forsi1@yahoo.com>
2021-07-18 16:56:03 +0500

    add back link to playgdourn

--- CSS/css-ui-page/pages/playground/buttons.js
+++ CSS/css-ui-page/pages/playground/buttons.js
@@ -1,14 +1,18 @@
-import styles from '../../styles/Buttons.module.css'
+import Link from 'next/link';
+import styles from '../../styles/Buttons.module.css';
 
 export default function buttons() {
   return (
     <div className={styles.container}>
+      <Link href='/'>
+        <p className={styles.back}>← Go Back</p>
+      </Link>
       <h2>Cool Buttons</h2>
-        <div className={styles.rotate}>Hover Me</div>
-        <div className={styles.rotateTwo}>Hover Me</div>
+      <div className={styles.rotate}>Hover Me</div>
+      <div className={styles.rotateTwo}>Hover Me</div>
       
       {/* swipe button: */}
-        <div className={styles.rotateThree}>Hover Me</div>
+      <div className={styles.rotateThree}>Hover Me</div>
     </div>
   )
 }
